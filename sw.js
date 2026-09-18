45a61d510775a68d4ee796f916443893d1d656a2
muhammadfisika <muhammad112@guru.sma.belajar.id>
2026-09-18 10:41:14 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,53 @@
+const CACHE_NAME = "scan-ljk-v1";
 
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+
+self.addEventListener(
+  "install",
+  function(event) {
+
+    event.waitUntil(
+
+      caches.open(CACHE_NAME)
+
+        .then(function(cache) {
+
+          return cache.addAll(
+            FILES_TO_CACHE
+          );
+
+        })
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  function(event) {
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .catch(function() {
+
+          return caches.match(
+            event.request
+          );
+
+        })
+
+    );
+
+  }
+);
